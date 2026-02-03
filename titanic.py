a443ae959c81827c2a5819aba535e01ca5566e86
simensae1 <tsang.simeon@gmail.com>
2026-02-03 08:47:28 +0000

    clean

--- titanic.py
+++ titanic.py
@@ -12,19 +12,19 @@ from sklearn.compose import ColumnTransformer
 from sklearn.metrics import confusion_matrix
 
 
-os.chdir('application_mise_en_prod_proj_ds/')
-TrainingData = pd.read_csv('data.csv')
+os.chdir("application_mise_en_prod_proj_ds/")
+TrainingData = pd.read_csv("data.csv")
 
 TrainingData.head()
 
 
-TrainingData['Ticket'].str.split("/").str.len()
+TrainingData["Ticket"].str.split("/").str.len()
 
-TrainingData['Name'].str.split(",").str.len()
+TrainingData["Name"].str.split(",").str.len()
 
 n_trees = 20
 max_depth = None
-max_features = 'sqrt'
+max_features = "sqrt"
 
 TrainingData.isnull().sum()
 
@@ -33,50 +33,66 @@ TrainingData.isnull().sum()
 # Statut socioéconomique
 
 
-fig, axes = plt.subplots(1, 2, figsize=(12, 6))  # layout matplotlib 1 ligne 2 colonnes taile 16*8
-fig1_pclass = sns.countplot(data=TrainingData, x="Pclass", ax=axes[0]).set_title("fréquence des Pclass")
-fig2_pclass = sns.barplot(data=TrainingData, x = "Pclass", y = "Survived", ax=axes[1]).set_title("survie des Pclass")
+fig, axes = plt.subplots(
+    1, 2, figsize=(12, 6)
+)  # layout matplotlib 1 ligne 2 colonnes taile 16*8
+fig1_pclass = sns.countplot(data=TrainingData, x="Pclass", ax=axes[0]).set_title(
+    "fréquence des Pclass"
+)
+fig2_pclass = sns.barplot(
+    data=TrainingData, x="Pclass", y="Survived", ax=axes[1]
+).set_title("survie des Pclass")
 
 
 # Age
 
-sns.histplot(data= TrainingData, x='Age',bins=15, kde=False    )    .set_title("Distribution de l'âge")
+sns.histplot(data=TrainingData, x="Age", bins=15, kde=False).set_title(
+    "Distribution de l'âge"
+)
 plt.show()
 
 ## Encoder les données imputées ou transformées.
 
-numeric_features=["Age", "Fare"]
-categorical_features=["Embarked", "Sex"]
+numeric_features = ["Age", "Fare"]
+categorical_features = ["Embarked", "Sex"]
 
-numeric_transformer = Pipeline(steps=[("imputer", SimpleImputer(strategy="median")),
-("scaler", MinMaxScaler()),])
+numeric_transformer = Pipeline(
+    steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", MinMaxScaler()),
+    ]
+)
 
-categorical_transformer = Pipeline(steps=[("imputer", SimpleImputer(strategy="most_frequent")),("onehot", OneHotEncoder()),])
+categorical_transformer = Pipeline(
+    steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder()),
+    ]
+)
 
 
 preprocessor = ColumnTransformer(
-transformers=[
-("Preprocessing numerical", numeric_transformer, numeric_features),
-(
-"Preprocessing categorical",
-categorical_transformer,
-categorical_features,
-),
-        ]
-    )
+    transformers=[
+        ("Preprocessing numerical", numeric_transformer, numeric_features),
+        (
+            "Preprocessing categorical",
+            categorical_transformer,
+            categorical_features,
+        ),
+    ]
+)
 
 pipe = Pipeline(
-        [
-            ("preprocessor", preprocessor),
-            ("classifier", RandomForestClassifier(n_estimators=20)),
-        ]
-    )
-
+    [
+        ("preprocessor", preprocessor),
+        ("classifier", RandomForestClassifier(n_estimators=20)),
+    ]
+)
 
 
 # splitting samples
 y = TrainingData["Survived"]
-X = TrainingData.drop("Survived", axis = 'columns')
+X = TrainingData.drop("Survived", axis="columns")
 
 # On _split_ notre _dataset_ d'apprentisage pour faire de la validation croisée une partie pour apprendre une partie pour regarder le score.
 # Prenons arbitrairement 10% du dataset en test et 90% pour l'apprentissage.
@@ -90,17 +106,19 @@ jetonapi = "$trotskitueleski1917"
 # Random Forest
 
 
-
-#Ici demandons d'avoir 20 arbres
+# Ici demandons d'avoir 20 arbres
 pipe.fit(X_train, y_train)
 
 
-#calculons le score sur le dataset d'apprentissage et sur le dataset de test (10% du dataset d'apprentissage mis de côté)
+# calculons le score sur le dataset d'apprentissage et sur le dataset de test (10% du dataset d'apprentissage mis de côté)
 # le score étant le nombre de bonne prédiction
 rdmf_score = pipe.score(X_test, y_test)
 rdmf_score_tr = pipe.score(X_train, y_train)
 print(f"{rdmf_score:.1%} de bonnes réponses sur les données de test pour validation")
 
-print(20*"-")
+print(20 * "-")
 print("matrice de confusion")
 print(confusion_matrix(y_test, pipe.predict(X_test)))
+
+
+# ruff check titanic.py
